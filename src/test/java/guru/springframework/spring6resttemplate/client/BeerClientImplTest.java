8cc10991ef310b23c3678e203b7bb90bdfbcee71
fusan <fusanfilm@163.com>
2024-02-26 23:31:13 +0800

    adding query param

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -11,11 +11,19 @@ class BeerClientImplTest {
 
     @Autowired
     BeerClientImpl beerClient;
+
+    @Test
+    void listBeersNoBeerName() {
+        beerClient.listBeers(null);
+    }
+
     @Test
     void listBeers() {
 
-        beerClient.listBeers();
+        beerClient.listBeers("ALE");
     }
+
+
 
 
 
